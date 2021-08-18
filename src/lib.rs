c1ddc6c5f3a4cba86a6231e6e3c8c2afbb320c86
Sirignus <lordignus@outlook.fr>
2021-08-19 01:03:21 +0200

    Added Texture support. Needs refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,10 @@ use self::glfw::{Context, Key, Action};
 
 extern crate gl;
 use gl::types::*;
+use image::GenericImageView;
+use image::Pixel;
+
+extern crate image;
 
 use std::sync::mpsc::Receiver;
 use std::ffi::CString;
@@ -10,6 +14,7 @@ use std::ptr;
 use std::str;
 use std::mem;
 use std::os::raw::c_void;
+use std::path::Path;
 
 // settings
 const DEFAULT_WIDTH: u32 = 800;
@@ -17,19 +22,181 @@ const DEFAULT_HEIGHT: u32 = 600;
 
 const vertexShaderSource: &str = r#"
     #version 330 core
-    layout (location = 0) in vec3 aPos;
+    layout (location = 0) in vec3 iPos;
+    layout (location = 1) in vec2 iTexCoord;
+
+    out vec2 texCoord;
+
     void main() {
-       gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+        gl_Position = vec4(iPos, 1.0);
+        texCoord = vec2(iTexCoord.x, iTexCoord.y);
     }
 "#;
 
 const fragmentShaderSource: &str = r#"
     #version 330 core
     out vec4 FragColor;
+
+    in vec2 texCoord;
+
+    uniform sampler2D texImg;
+
     void main() {
-       FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);
+       FragColor = texture(texImg, texCoord);
     }
 "#;
+
+pub struct Vertex {
+    position: [f32; 3],
+    texture_coord: [f32; 2],
+}
+
+impl Vertex {
+    pub fn new(position: Vector3, texture_coord: [f32; 2]) -> Vertex {
+        Vertex {
+            position: [
+                position.x,
+                position.y,
+                position.z,
+            ],
+            texture_coord: texture_coord,
+        }
+    }
+}
+#[derive(Debug, Default, Clone, Copy)]
+pub struct Vector3 {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+impl Vector3 {
+    pub fn new(x: f32, y: f32, z:f32) -> Vector3 {
+        Vector3 {
+            x: x,
+            y: y,
+            z: z,
+        }
+    }
+
+    pub fn from_xy(x: f32, y: f32) -> Vector3 {
+        Vector3 {
+            x: x,
+            y: y,
+            z: 0.0,
+        }
+    }
+}
+
+#[derive(Debug, Default, Clone)]
+pub struct Quad {
+    texture: u32,
+    vertices: Vec<Vector3>,
+    vbo: u32,
+    vao: u32,
+    ebo: u32,
+
+}
+
+impl Quad {
+    pub fn new(positions: Vec<Vector3>, texture: String) -> Quad {
+        if positions.len() != 4 || texture.is_empty() {
+            panic!("Invalid values for Quad creation");
+        }
+
+        let vertices: [Vertex; 4] = [
+            Vertex::new(positions[0], [0.0, 1.0]),
+            Vertex::new(positions[1], [1.0, 1.0]),
+            Vertex::new(positions[2], [1.0, 0.0]),
+            Vertex::new(positions[3], [0.0, 0.0]),
+        ];
+        let indices: [u32; 6] = [
+            0, 1, 2,  // first Triangle
+            2, 3, 0   // second Triangle
+        ];
+        
+        let mut texture : u32 = 0;
+        let (mut vbo, mut vao, mut ebo) = (0, 0, 0);
+        unsafe {
+
+            gl::GenVertexArrays(1, &mut vao);
+            gl::GenBuffers(1, &mut vbo);
+            gl::GenBuffers(1, &mut ebo);
+
+            gl::BindVertexArray(vao);
+
+            gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+            gl::BufferData(gl::ARRAY_BUFFER,
+                        (vertices.len() * mem::size_of::<Vertex>()) as GLsizeiptr,
+                        &vertices as *const _ as *const c_void,
+                        gl::STATIC_DRAW);
+
+            gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ebo);
+            gl::BufferData(gl::ELEMENT_ARRAY_BUFFER,
+                        (indices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+                        &indices[0] as *const u32 as *const c_void,
+                        gl::STATIC_DRAW);
+            
+            let stride =  mem::size_of::<Vertex>() as GLsizei;
+            // position attribute
+            gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, stride, ptr::null());
+            gl::EnableVertexAttribArray(0);
+            // texture coord attribute
+            gl::VertexAttribPointer(1, 2, gl::FLOAT, gl::FALSE, stride, (3 * mem::size_of::<GLfloat>()) as *const c_void);
+            gl::EnableVertexAttribArray(1);
+        
+
+
+            // Generate the texture and get the id/address
+            gl::GenTextures(1, &mut texture);
+
+            // Set texture filtering options
+            gl::BindTexture(gl::TEXTURE_2D, texture);
+            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::REPEAT as i32); // set texture wrapping to gl::REPEAT (default wrapping method)
+            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::REPEAT as i32);
+            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR as i32);
+            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
+
+            // load image, create texture and generate mipmaps
+            let img = image::open(&Path::new("Assets/rust.png")).expect("Failed to load texture").flipv();
+            let (width, height) = img.dimensions();
+            let rgba_image = img.as_rgba8().expect("Failed to convert texture to rgba8");
+            let data = rgba_image.as_raw();
+            
+            
+            gl::TexImage2D(gl::TEXTURE_2D,
+                        0, // mipmap level
+                        gl::RGB as i32,
+                        width as i32,
+                        height as i32,
+                        0,
+                        gl::RGBA,
+                        gl::UNSIGNED_BYTE,
+                        &data[0] as *const u8 as *const c_void);
+            gl::GenerateMipmap(gl::TEXTURE_2D);
+        }
+        
+
+        Quad {
+            texture: texture,
+            vertices: positions,
+            vbo: vbo,
+            vao: vao,
+            ebo: ebo,
+        }
+    }
+
+    pub unsafe fn draw(& self, shader_program: u32) {
+        // Bind Texture
+        
+        gl::ActiveTexture(self.texture);
+        gl::BindTexture(gl::TEXTURE_2D, self.texture);
+
+        gl::UseProgram(shader_program);
+        gl::BindVertexArray(self.vao); 
+        gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, ptr::null());
+    }
+}
 
 
 pub fn run() {
@@ -135,6 +302,16 @@ pub fn run() {
         (shaderProgram, VAO)
     };
 
+    let positions = vec![
+        Vector3::from_xy(-0.7, 0.7),
+        Vector3::from_xy(0.7, 0.7),
+        Vector3::from_xy(0.7, -0.7),
+        Vector3::from_xy(-0.7, -0.7),
+    ];
+    let quad = Quad::new(positions, String::from("  "));
+
+    println!("{:#?}", quad);
+
     // render loop
     // -----------
     while !window.should_close() {
@@ -149,10 +326,12 @@ pub fn run() {
             gl::Clear(gl::COLOR_BUFFER_BIT);
 
             // draw our first triangle
-            gl::UseProgram(shaderProgram);
-            gl::BindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+            // gl::UseProgram(shaderProgram);
+            // gl::BindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+            // gl::DrawArrays(gl::TRIANGLES, 0, 3);
             // glBindVertexArray(0); // no need to unbind it every time
+
+            quad.draw(shaderProgram);
         }
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
